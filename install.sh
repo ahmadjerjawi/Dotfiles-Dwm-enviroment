794dc0d80f6f2ca2fdee371554ff03b368b80a6d
ahmadjerjawi <72747814+ahmadjerjawi@users.noreply.github.com>
2024-03-21 19:50:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
 # Check if script is running as sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as sudo"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as sudo"
   exit
 fi
 
-# Check for local and config files and move them to .local and .config
-for dir in local config; do
-  if [ -d "$dir" ] && [ ! -d ".$dir" ]; then
-    mv "$dir" ".$dir"
-  fi
-done
+# Define variables
+dotfiles_repo="https://github.com/ahmadjerjawi/Dotfiles-Dwm-envirmoent.git"
+progs_csv="https://raw.githubusercontent.com/ahmadjerjawi/Dotfiles-Dwm-envirmoent/main/progs.csv"
+aur_helper="yay"  # You can change this to your preferred AUR helper
+export TERM=ansi
+
+# Clone dotfiles repository
+sudo -u "$SUDO_USER" git clone --depth 1 "$dotfiles_repo" /tmp/dotfiles || exit 1
+cd /tmp/dotfiles || exit 1
+
+# Install local and config files
+mv local/* "$HOME/.local/"
+mv config/* "$HOME/.config/"
+chown -R "$SUDO_USER:$SUDO_USER" "$HOME/.local" "$HOME/.config"
 
 # Install dwm, dwmblocks, st, and dmenu
 for prog in dwm dwmblocks st dmenu; do
-  cd ~/.local/src/$prog
-  sudo make clean install
+  cd ~/.local/src/$prog || continue
+  sudo -u "$SUDO_USER" make clean install
 done
 
-# Remove specified programs
-sudo pacman -Rns librewolf newsboat abook lf neomutt
+# Install programs listed in progs.csv
+if [ ! -f /tmp/progs.csv ]; then
+  curl -Ls "$progs_csv" > /tmp/progs.csv || exit 1
+fi
 
-# Install specified programs
-sudo pacman -S chromium nemo ttf-nerd-fonts-symbols atuin
+while IFS=, read -r tag program comment; do
+  case "$tag" in
+    "A") sudo -u "$SUDO_USER" $aur_helper -S --noconfirm "$program" ;;
+    *) sudo pacman --noconfirm -S "$program" ;;
+  esac
+done < /tmp/progs.csv
+# Clean up
+rm -rf /tmp/dotfiles /tmp/progs.csv
+
+echo "Installation completed."
